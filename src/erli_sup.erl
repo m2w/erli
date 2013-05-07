69a7ff4b461548c469b0853ec315d87d2e7f557f
Moritz <moritz@tibidat.com>
2013-05-07 22:31:31 +0900

    Cleans up the webmachine configuration.

--- src/erli_sup.erl
+++ src/erli_sup.erl
@@ -36,21 +36,17 @@ upgrade() ->
 %%-----------------------------------------------------------
 
 init([]) ->
-    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
+    {ok, Ip} = application:get_env(webmachine, ip),
+    {ok, Port} = application:get_env(webmachine, port),
     {ok, App} = application:get_application(?MODULE),
     {ok, Dispatch} = file:consult(filename:join([erli_utils:priv_dir(App),
                                                  "dispatch.conf"])),
-    Port = case os:getenv("WEBMACHINE_PORT") of
-	       false -> 8000;
-	       AnyPort -> AnyPort
-	   end,
-    WebConfig = [
-                 {ip, Ip},
-                 {port, Port},
-                 {log_dir, "priv/log"},
-                 {dispatch, Dispatch}],
-    Web = {webmachine_mochiweb,
-           {webmachine_mochiweb, start, [WebConfig]},
-           permanent, 5000, worker, [mochiweb_socket_server]},
-    Processes = [Web],
+    Config = [{ip, Ip},
+	      {port, Port},
+	      {log_dir, "priv/log"},
+	      {dispatch, Dispatch}],
+    Webmachine = {webmachine_mochiweb,
+		  {webmachine_mochiweb, start, [Config]},
+		  permanent, 5000, worker, [mochiweb_socket_server]},
+    Processes = [Webmachine],
     {ok, {{one_for_one, 10, 10}, Processes}}.
