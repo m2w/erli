b1f195c10f5bf27b9297167bc905e1cf51c23843
Moritz <moritz@tibidat.com>
2013-05-09 16:03:49 +0900

    Improves target record  generation and adds support for generating path records.

--- ct/test_utils.erl
+++ ct/test_utils.erl
@@ -9,7 +9,8 @@
 
 %% API
 -export([clear_all_mnesia_data/0,
-	 generate_targets/1]).
+	 generate_targets/1,
+	 generate_paths/1]).
 
 -include("models.hrl").
 
@@ -25,21 +26,44 @@ clear_all_mnesia_data() ->
     mnesia:clear_table(paths),
     mnesia:clear_table(visits).
 
-generate_targets(0) ->
-    ok;
-generate_targets(Count) ->
+generate_paths(N) ->
+    Targets = generate_targets(random:uniform(N)),
+    generate_paths(N, Targets, []).
+
+generate_paths(0, Targets, Acc) ->
+    Acc;
+generate_paths(N, Targets, Acc) ->
+    X = random:uniform(length(Targets)),
+    Id = (lists:nth(X, Targets))#target.id,
+    case erli_storage:create(#path{target_id=Id}) of
+	{error, Error} ->
+	    error_logger:info_msg("error = ~s", [Error]),
+	    generate_paths(N, Targets, Acc);
+	P ->
+	    generate_paths(N - 1, Targets, [P|Acc])
+    end.
+
+generate_targets(N) ->
+    generate_targets(N, []).
+
+generate_targets(0, Acc) ->
+    Acc;
+generate_targets(N, Acc) ->
+    Target = make_target(),
+    case erli_storage:create(Target) of
+	{error, _} ->
+	    generate_targets(N, Acc);
+	T ->
+	    generate_targets(N - 1, [T|Acc])
+    end.
+
+make_target() ->
     {ok, FL} = application:get_env(erli, flag_limit),
     Domain = generate_domain(),
     TLD = generate_tld(),
     Url = <<"http://", Domain/bitstring, ".", TLD/bitstring>>,
     FC = random:uniform(25),
-    Target = #target{url=Url, flag_count=FC, is_banned=FC>FL},
-    case erli_storage:create(Target) of
-	{error, _} ->
-	    generate_targets(Count);
-	_Rec ->
-	    generate_targets(Count - 1)
-    end.
+    #target{url=Url, flag_count=FC, is_banned=FC>FL}.
 
 
 generate_domain() ->
