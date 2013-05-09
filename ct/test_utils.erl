6004b34aae1644c36251ee1604dc229d3ddc0934
Moritz <moritz@tibidat.com>
2013-05-09 16:33:23 +0900

    Refactors the target resource test helper methods into the test_utils module.

--- ct/test_utils.erl
+++ ct/test_utils.erl
@@ -10,9 +10,15 @@
 %% API
 -export([clear_all_mnesia_data/0,
 	 generate_targets/1,
-	 generate_paths/1]).
+	 generate_paths/1,
+	 validate_meta/6,
+	 post_request/2,
+	 build_request/4,
+	 build_request/3,
+	 build_request/2]).
 
 -include("models.hrl").
+-include_lib("common_test/include/ct.hrl").
 
 %%-----------------------------------------------------------
 %% API Methods
@@ -26,11 +32,47 @@ clear_all_mnesia_data() ->
     mnesia:clear_table(paths),
     mnesia:clear_table(visits).
 
+
 generate_paths(N) ->
     Targets = generate_targets(random:uniform(N)),
     generate_paths(N, Targets, []).
 
-generate_paths(0, Targets, Acc) ->
+
+generate_targets(N) ->
+    generate_targets(N, []).
+
+
+validate_meta(TotalSize, ObjCount, RangeStart, RangeEnd, MaxOffset, Meta) ->
+    TotalSize = proplists:get_value(<<"totalCollectionSize">>, Meta),
+    ObjCount = proplists:get_value(<<"objectCount">>, Meta),
+    RangeStart = proplists:get_value(<<"rangeStart">>, Meta),
+    RangeEnd = proplists:get_value(<<"rangeEnd">>, Meta),
+    MaxOffset = proplists:get_value(<<"maxCollectionOffset">>, Meta).
+
+
+post_request(Payload, Config) ->
+    httpc:request(post, {?config(root_url, Config), [],
+			 "application/json", Payload}, [], []).
+
+
+build_request(Method, Config) ->
+    build_request(Method, "", [], Config).
+
+
+build_request(Method, Id, Config) when is_binary(Id) ->
+    build_request(Method, binary_to_list(Id), [], Config);
+build_request(Method, Headers, Config) when is_list(Headers) ->
+    build_request(Method, "", Headers, Config).
+
+
+build_request(Method, Id, Headers, Config) ->
+    httpc:request(Method, {?config(root_url, Config) ++ Id, Headers}, [], []).
+
+%%----------------------------------------------------------
+%% Internal methods
+%%----------------------------------------------------------
+
+generate_paths(0, _Targets, Acc) ->
     Acc;
 generate_paths(N, Targets, Acc) ->
     X = random:uniform(length(Targets)),
@@ -43,8 +85,6 @@ generate_paths(N, Targets, Acc) ->
 	    generate_paths(N - 1, Targets, [P|Acc])
     end.
 
-generate_targets(N) ->
-    generate_targets(N, []).
 
 generate_targets(0, Acc) ->
     Acc;
@@ -56,6 +96,7 @@ generate_targets(N, Acc) ->
 	T ->
 	    generate_targets(N - 1, [T|Acc])
     end.
+
 
 make_target() ->
     {ok, FL} = application:get_env(erli, flag_limit),
@@ -70,6 +111,7 @@ generate_domain() ->
     generate_bin(random:uniform(30)).
 generate_tld() ->
     generate_bin(random:uniform(3)).
+
 
 generate_bin(Len) ->
     re:replace(base64:encode(crypto:rand_bytes(Len)),
