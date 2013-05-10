2829f1a36dfc66371deebeca20eebe5e0e29d253
Moritz <moritz@tibidat.com>
2013-05-10 18:36:19 +0900

    Tiny whitespace fix.

--- ct/target_resource_SUITE.erl
+++ ct/target_resource_SUITE.erl
@@ -22,6 +22,7 @@ init_per_suite(Config) ->
 
     application:set_env(mnesia, dir, Priv),
     application:set_env(webmachine, dispatch_dir, DataDir),
+
     erli:start(),
 
     {ok, DefaultOffset} = application:get_env(erli, default_collection_offset),
